5f8801647d226cbdb33334981fd7bd1c25ce9be6
agartanah <vyatin2017@mail.ru>
2023-03-12 13:54:04 +0300

    I created a text file class ClassFile with serialization and deserialization capability and tested its functionality

---
+++ ClassFile.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4 {
+
+  [Serializable]
+  public class ClassFile {
+    public string FileUserContent { get; set; }
+
+    public ClassFile() {
+      FileUserContent = default;
+    }
+
+    public ClassFile(FileStream FileUser) {
+      StreamReader SR = new StreamReader(FileUser);
+
+      while (SR.EndOfStream != true) {
+        FileUserContent += SR.ReadLine() + "\n";
+      }
+
+      SR.Close();
+    }
+
+    public ClassFile(string Path) {
+      FileStream FileUser = new FileStream(Path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+      StreamReader SR = new StreamReader(FileUser);
+
+      while (SR.EndOfStream != true) {
+        FileUserContent += SR.ReadLine() + "\n";
+      }
+
+      SR.Close();
+      FileUser.Close();
+    }
+
+    public void BinarySerialize(FileStream FileBinary) {
+      BinaryFormatter Binary = new BinaryFormatter();
+      Binary.Serialize(FileBinary, this);
+      FileBinary.Flush();
+      FileBinary.Close();
+    }
+
+    public void BinaryDeserialize(FileStream FileBinary) {
+      BinaryFormatter BinaryFile = new BinaryFormatter();
+      ClassFile DeserializedFile = (ClassFile)BinaryFile.Deserialize(FileBinary);
+
+      FileUserContent = DeserializedFile.FileUserContent;
+      FileBinary.Close();
+    }
+
+    public void XMLSerialization(FileStream FileXml) {
+      XmlSerializer Xml = new XmlSerializer(this.GetType());
+
+      Xml.Serialize(FileXml, this);
+      FileXml.Flush();
+      FileXml.Close();
+    }
+
+    public void XMLDeserialization(FileStream FileXml) {
+      XmlSerializer Xml = new XmlSerializer(typeof(ClassFile));
+      ClassFile DeserializedFile = (ClassFile)Xml.Deserialize(FileXml);
+
+      FileUserContent = DeserializedFile.FileUserContent;
+      FileXml.Close();
+    }
+  }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +11,51 @@ using System.Threading.Tasks;
 namespace Lab_4 {
   class Program {
     static void Main(string[] args) {
+      string Path = @"C:\Users\vyati\source\repos\Lab4\temp.txt";
+
+      FileStream FileUser = new FileStream(Path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+      ClassFile Filek = new ClassFile(FileUser);
+
+      Console.WriteLine(Filek.FileUserContent);
+
+      FileStream BinaryFile = new FileStream(@"C:\Users\vyati\source\repos\Lab4\tempb.bin",
+        FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+      Filek.BinarySerialize(BinaryFile);
+
+      BinaryFile.Close();
+
+      Filek = new ClassFile(@"C:\Users\vyati\source\repos\Lab4\temp2.txt");
+
+      BinaryFile = new FileStream(@"C:\Users\vyati\source\repos\Lab4\tempb.bin",
+        FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+      Console.WriteLine(Filek.FileUserContent);
+
+      Filek.BinaryDeserialize(BinaryFile);
+
+      Console.WriteLine(Filek.FileUserContent);
+
+      FileStream XmlFile = new FileStream(@"C:\Users\vyati\source\repos\Lab4\tempb.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+      Filek.XMLSerialization(XmlFile);
+
+
+      //MyFile.BinarySerialize(FileF);
+      //FileStream fs = new
+      //FileStream(@"C:\Users\vyati\source\repos\Lab4\FullNameSerialize.bin",
+      //FileMode.OpenOrCreate, FileAccess.Write);
+      //FullNameClass fnc = new FullNameClass("Ivan", "Ivanov", "Ivanovich");
+      //fnc.Print();
+      //fnc.Serialize(fs);
+      //fnc = new FullNameClass("Petr", "Petrov", "Petrovich");
+      //fnc.Print();
+      //fs = new
+      //FileStream(@"C:\Users\vyati\source\repos\Lab4\FullNameSerialize.bin",
+      //FileMode.OpenOrCreate, FileAccess.Read);
+      //fnc.Deserialize(fs);
+      //fnc.Print();
     }
   }
 }
