75873857cc8864587f3511067bde77d1fa5c4fe7
agartanah <vyatin2017@mail.ru>
2023-03-12 18:25:38 +0300

    Created the SearchFiles class to search for text files by keywords and checked its performance

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -11,36 +10,14 @@ using System.Threading.Tasks;
 namespace Lab_4 {
   class Program {
     static void Main(string[] args) {
-      string Path = @"C:\Users\vyati\source\repos\Lab_4\temp.txt";
 
-      FileStream FileUser = new FileStream(Path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+      List<string> ListFiles;
 
-      ClassFile Filek = new ClassFile(FileUser);
+      ListFiles = SearchFiles.FilesKeyWordSearch(@"C:\Users\vyati\source\repos\Lab_4\", "alone");
 
-      Console.WriteLine(Filek.FileUserContent);
-
-      FileStream BinaryFile = new FileStream(@"C:\Users\vyati\source\repos\Lab_4\tempb.bin",
-        FileMode.OpenOrCreate, FileAccess.ReadWrite);
-
-      Filek.BinarySerialize(BinaryFile);
-
-      BinaryFile.Close();
-
-      Filek = new ClassFile(@"C:\Users\vyati\source\repos\Lab_4\temp2.txt");
-
-      BinaryFile = new FileStream(@"C:\Users\vyati\source\repos\Lab_4\tempb.bin",
-        FileMode.OpenOrCreate, FileAccess.ReadWrite);
-
-      Console.WriteLine(Filek.FileUserContent);
-
-      Filek.BinaryDeserialize(BinaryFile);
-
-      Console.WriteLine(Filek.FileUserContent);
-
-      FileStream XmlFile = new FileStream(@"C:\Users\vyati\source\repos\Lab_4\tempb.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-
-      Filek.XMLSerialization(XmlFile);
-
+      foreach (var ListElement in ListFiles) {
+        Console.WriteLine(ListElement);
+      }
 
       //MyFile.BinarySerialize(FileF);
       //FileStream fs = new
---
+++ SearchFiles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4 {
+  class SearchFiles {
+    public static List<string> FilesKeyWordSearch(string DirectoryPath, string KeyWords) {
+      List<string> ListResult = new List<string>();
+      var FilesInDirectoryTXT = Directory.EnumerateFiles(DirectoryPath, "*.txt", SearchOption.AllDirectories);
+
+      foreach (var FileTXT in FilesInDirectoryTXT) {
+        string FileTXTTitle = FileTXT.Substring(DirectoryPath.Length);
+        if (File.ReadLines(DirectoryPath + FileTXTTitle).Any(line => line.Contains(KeyWords)) 
+          || FileTXTTitle.Contains(KeyWords)) {
+          ListResult.Add(FileTXTTitle);
+        }
+      }
+
+      return ListResult;
+    }
+  }
+}
